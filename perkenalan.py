6792fb772d77357019d094829c22c5099e7aefaa
Victor <140993993+VictorGirsang@users.noreply.github.com>
2024-01-15 13:38:48 +1100

    Add files via upload

---
+++ perkenalan.py
@@ -0,0 +1,6 @@
+nama = "Victor"
+kelas = "X-A"
+jurusan ="TKJ"
+
+print("perkenalkan nama saya", nama)
+print(" saya kelas ", kelas,"dari jurusan", jurusan ) 
---
+++ variable.py
@@ -0,0 +1,18 @@
+x = 7
+x = 2
+z = "SMK MEDIKACOM"
+P = False
+q = True
+
+print(x)
+print(x)
+print(z)
+print(P)
+print(q)
+
+print("cek tipe data")
+print(type(x))
+print(type(x))
+print(type(z))
+print(type(P))
+print(type(q))
